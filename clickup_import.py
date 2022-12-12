6cfd55f75676c1d071de44c4f28097d640a41718
Miguel Vieira <jmvieira@pm.me>
2022-12-12 09:05:16 +0000

    fix(clickup): add missing comment text
    When a comment was posted by a non-clickup user, the text of the comment was being dropped.

--- clickup_import.py
+++ clickup_import.py
@@ -643,7 +643,7 @@ def import_ac_task(
 
         text = comment["body_plain_text"]
         if not token:
-            text = f"Originally posted by {comment['created_by_name']}"
+            text = f"Originally posted by {comment['created_by_name']}\n{text}"
 
         task_comment_map[comment["id"]] = comment["parent_id"]
         clickup.get_or_create_comment(task["id"], text, token)
